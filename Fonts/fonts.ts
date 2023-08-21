2489b282f61e082abf5fdca52d36bcded0178372
GOTTA5786 <coolkote5786@gmail.com>
2023-08-21 17:49:09 +0300

    first container on main page

---
+++ Fonts/fonts.ts
@@ -0,0 +1,18 @@
+import { Oswald, Cormorant_Garamond, Arizonia } from 'next/font/google'
+ 
+export const cormotant = Cormorant_Garamond({
+    weight: ["300"],
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const oswaldo = Oswald({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const arizonia = Arizonia({
+    weight: ["400"],
+    subsets: ['latin'],
+    display: 'swap',
+  })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 import styles from './page.module.css'
+import MainFlover1 from '@/public/Main-flover1.png' 
+import { cormotant,oswaldo,arizonia } from '@/Fonts/fonts'
 
 export default function Home() {
   return (
     <>
-      <p className={styles.text}>Hello world</p>
+      <div className={styles.container1}>
+        <p className={styles.storeName1 + " " + cormotant.className}>lover</p>
+        <p className={styles.storeName2 + " " + cormotant.className}>flower</p>
+        <p className={styles.slogan + " " + oswaldo.className}>Создаём для тех, кто ценит свежесть и изящество цветка</p>
+        <Image src = {MainFlover1}
+          alt = 'default'
+          className={styles.MainFlover1}
+          layout='fill'
+          objectFit='cover'></Image>
+        <div className={styles.purpleEffect}></div>
+        <div className={styles.blueEffect}></div> 
+        <div className={styles.containerWithShadow}></div>
+        <p className={styles.bgTextLF + " " + arizonia.className}>lover flower</p>
+      </div>
+      
     </>
   )
 }
