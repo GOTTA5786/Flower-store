4e2db32b321a2f8b23dff2e3bdba246db17e4f1d
GOTTA5786 <coolkote5786@gmail.com>
2023-08-22 23:43:23 +0300

    added vercel postreSQL DB

--- components/PopularContainer/PopularContainer.tsx
+++ components/PopularContainer/PopularContainer.tsx
@@ -4,6 +4,15 @@ import Image from 'next/image'
 import PopularBg from '@/public/PopularBg.png'
 import PopularItems from '../PopularItems/PopularItems'
 
+const testFlower:object[] = [{
+  flower_id: 1,
+  title: 'Золотое сердце осени',
+  price: '$432.40',
+  saleprice: '$300.00',
+  sale: true,
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aspernatur corporis ipsa eveniet, provident quia inventore officia deleniti accusamus repudiandae deserunt alias, fuga voluptate voluptatibus rem tenetur labore dicta quas?',
+  pathtoimg: 'flower_1'
+}]
 
 export default function PopularContainer() {
   return (
